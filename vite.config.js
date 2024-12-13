0fdba4f8556a1b17cfe3757759cd79c783efb6ed
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-12-13 19:20:59 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: 'src', // или путь к вашей папке с исходниками
+  build: {
+    outDir: '../dist' // или другой путь для сборки
+  },
+  server: {
+    host: '0.0.0.0', // чтобы приложение было доступно по сети
+    port: 5174 // или другой порт, если нужно
+  }
+});
+
