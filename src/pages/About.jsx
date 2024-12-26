fabeab4a3e05774624295a76d817eaf34f280fd8
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-12-26 16:38:58 +0300

    updates

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+
+    return (
+        <div className='legal-page'>
+            <div className="legal-container container pt-4">
+                <h5>О компании</h5>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Delivery.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Delivery = () => {
+
+    return (
+        <div className='legal-page'>
+            <div className="legal-container container pt-4">
+                <h5>Доставка</h5>
+
+            </div>
+        </div>
+    )
+}
+
+export default Delivery
