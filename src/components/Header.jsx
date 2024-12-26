c794d2da4eb088f4ac06aacfa92c67df508caac4
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-12-26 16:45:12 +0300

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,7 +88,7 @@ const Header = () => {
                                     <Link to={'/legal'} className="side-link me-3" onClick={() => setShowMobile(false)}>Правовая информация</Link><br />
                                     <a href='mailto:Saha.inna77@mail.ru' className="side-link me-3">Написать нам</a>
                                     <br />
-                                    <a href='tel:+79777520413' className="side-link">Позвонить</a>
+                                    <a href='tel:+79777520413' className="side-link">Позвонить</a> <br />
                                     <Link to={'/delivery'} className="side-link me-3" onClick={() => setShowMobile(false)}>Доставка</Link> <br />
                                     {/* <div className="side-link">Информация для покупателей</div> */}
                                 </div>
